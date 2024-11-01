d24a259a3ff0f215801e77686bf851ff85053cc0
Ezequiel Bugnon <bugnonezequiel@gmail.com>
2024-11-01 17:15:26 -0300

    deleted code incorrect

--- script/main.go
+++ script/main.go
@@ -7,15 +7,9 @@ import (
 	"os/exec"
 	"script/fetch"
 	"strings"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
 
 	urlCallback := os.Getenv("URLCALLBACK")
 	urlExecution := os.Getenv("URLEXECUTION")
